52f90158d20b6b17ae9a77680094e17b5ff22b3e
JSavage42 <jasavage42@gmail.com>
2019-02-17 11:48:05 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      plain: '',
+      key: '',
+    };
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.plain + (this.state.key % 26));
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { plain, key } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Vigenère Cipher</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="plain"
+            onChange={this.onChange}
+            value={plain}
+            placeholder="Plain"
+          />
+          <input
+            type="text"
+            name="key"
+            onChange={this.onChange}
+            value={key}
+            placeholder="Key"
+          />
+          <select name="" onChange={this.onChange}>
+            <option value="encode">Encode</option>
+            <option value="decode">Decode</option>
+          </select>
+          <input type="submit" value="Run" />
+          <input type="reset" />
+        </form>
       </div>
     );
   }
